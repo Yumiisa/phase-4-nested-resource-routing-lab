ac40257aa5d4f9a62cba52df08e022dc02dc2df9
Gaudencia <conny@students.uonbi.ac.ke>
2022-10-01 02:26:19 +0300

    commit tree b3363760a71c7ad7fa58ed749ac4e9d16506798d

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
+    
     items = Item.all
     render json: items, include: :user
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
+  resources:users, only: [:index, :show] do
+  resources :items, only: [:index ] 
+    
+  end
   resources :users, only: [:show]
 end
